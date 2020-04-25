b215633142c5fa6c1f187d90a234c971313f5585
guanz1 <guanz1@ziroom.com>
2020-04-25 17:07:09 +0800

    代码初始化提交

---
+++ app/src/main/java/com/z/todaydemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.z.todaydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.z.refresh.GodRefreshLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GodRefreshLayout refreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        refreshLayout = (GodRefreshLayout) findViewById(R.id.refresh);
+        refreshLayout.setRefreshManager();
+    }
+}
